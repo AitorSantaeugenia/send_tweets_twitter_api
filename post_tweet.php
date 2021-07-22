92eea4ab375627fd4f401992b743a5ddf43e6d92
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-07-22 18:43:16 +0200

    add POST tweet method

---
+++ post_tweet.php
@@ -0,0 +1,31 @@
+<?php
+    //include config.php and php wrapper for twitter
+    require_once( 'config.php' );
+    require_once( 'TwitterAPIExchange.php' );
+
+    //settings for the connection
+    $settings = array(
+        'oauth_access_token' => TWITTER_ACCESS_TOKEN,
+        'oauth_access_token_secret' => TWITTER_ACCESS_TOKEN_SECRET,
+        'consumer_key' => TWITTER_CONSUMER_KEY,
+        'consumer_secret' => TWITTER_CONSUMER_SECRET,
+    );
+
+    // URL and request method
+    $url = 'https://api.twitter.com/1.1/statuses/update.json';
+    $requestMethod = 'POST';
+
+    $apiData = array(
+        'status' => "Type the tweet you want to POST.",
+    );
+
+    // communication and API call
+    $twitter = new TwitterAPIExchange( $settings );
+    $twitter->buildOauth( $url, $requestMethod );
+    $twitter->setPostfields( $apiData );
+    $response = $twitter->performRequest( true, array( CURLOPT_SSL_VERIFYHOST => 0, CURLOPT_SSL_VERIFYPEER => 0 ) );
+
+    echo '<pre>';
+    print_r( json_decode( $response, true ) );
+
+?>
