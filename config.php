fd93de51e66f3a6e090184aa760cff70951d2d62
Aitor J. Santaeugenia <aitorsantaeugenia@gmail.com>
2021-07-22 05:41:38 +0200

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    define('TWITTER_CONSUMER_KEY', 'your_consumer_key');
-    define('TWITTER_CONSUMER_SECRET', 'yours');
-    define('TWITTER_ACCESS_TOKEN', 'yours');
-    define('TWITTER_ACCESS_TOKEN_SECRET', 'yours');
