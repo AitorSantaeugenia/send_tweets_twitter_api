f9a9b25baaf3f7e958cce89e8c70e79bb069b0bd
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-07-22 18:25:02 +0200

    grammar

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    define('TWITTER_CONSUMER_KEY', 'your_consumer_key');
+    define('TWITTER_CONSUMER_KEY', 'your_key');
     define('TWITTER_CONSUMER_SECRET', 'your_key');
     define('TWITTER_ACCESS_TOKEN', 'your_key');
     define('TWITTER_ACCESS_TOKEN_SECRET', 'your_key');
